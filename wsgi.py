ed7b4a5c9b2862ec43495c5a3072bbd74344d960
Michael Bond <bond@datawaslost.net>
2018-02-13 16:01:34 +0000

    Test view, use underscore instead of hyphen

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'newto-django.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'newto_django.settings'
 
 from django.core.wsgi import get_wsgi_application
 
