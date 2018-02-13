d65c0c729e130ea0d07c838509a8da8de3e974dc
Michael Bond <bond@datawaslost.net>
2018-02-13 16:18:56 +0000

    Clean up Model keys

--- models.py
+++ models.py
@@ -9,9 +9,9 @@ class Profile(models.Model):
 	hometown = models.CharField(max_length=30, blank=True)
 	joined = models.DateField(auto_now_add=True)
 	last_change = models.DateField(auto_now=True)
-	school = models.ForeignKey(School)
-	todo = models.ManyToManyField(Item, through='Todo')
-	bookmarks = models.ManyToManyField(Item, through='Bookmark')
+	school = models.ForeignKey('School', on_delete=models.SET_NULL, blank=True, null=True)
+	todo = models.ManyToManyField('Item', through='Todo')
+	bookmarks = models.ManyToManyField('Item', through='Bookmark')
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
@@ -25,19 +25,19 @@ def save_user_profile(sender, instance, **kwargs):
 
 class Metro(models.Model):
 	name = models.CharField(max_length=128, unique=True)
-	default_items = models.ManyToManyField(Item)
-	discover_items = models.ManyToManyField(Item)
-	tips = models.ManyToManyField(Tip)
+	default_items = models.ManyToManyField('Item')
+	discover_items = models.ManyToManyField('Item')
+	tips = models.ManyToManyField('Tip')
 
 
 class School(Metro):
-	metro = models.ForeignKey(Metro)
+	metro = models.ForeignKey('Metro', on_delete=models.SET_NULL, blank=True, null=True)
 	public = models.BooleanField(default=True)
 
 
 class ProspectiveUser(models.Model):
 	email = models.EmailField(unique=True)
-	school = models.ForeignKey(School)
+	school = models.ForeignKey('School', on_delete=models.CASCADE)
 
 
 class Tip(models.Model):
@@ -53,31 +53,31 @@ class Item(models.Model):
 	sponsor = models.CharField(max_length=128)
 	link = models.URLField()
 	image = models.ImageField()
-	ctas = models.ManyToManyField(Cta)
+	ctas = models.ManyToManyField('Cta')
 
 	def __str__(self):
 		return self.name
 
 
 class Group(Item):
-	items = models.ManyToManyField(Item, symmetrical=False)
+	items = models.ManyToManyField('Item', symmetrical=False)
 
 
 class Place(Item):
-	metro = models.ForeignKey(Metro)
+	metro = models.ForeignKey('Metro', on_delete=models.SET_NULL, blank=True, null=True)
 	address = models.CharField(max_length=128)
 	hours = models.CharField(max_length=128)
 	phone = models.CharField(max_length=128)
 	featured = models.BooleanField(default=False)
-	category = models.ForeignKey(Category)
-	tags = models.ManyToManyField(Tag)
-	ratings = models.ManyToManyField(Profile, through='Rating')
+	category = models.ForeignKey('Category', on_delete=models.SET_NULL, blank=True, null=True)
+	tags = models.ManyToManyField('Tag')
+	ratings = models.ManyToManyField('Profile', through='Rating')
 
 
 class Category(models.Model):
 	name = models.CharField(max_length=128, unique=True)
 	image = models.ImageField()
-	tags = models.ManyToManyField(Tag)
+	tags = models.ManyToManyField('Tag')
 
 
 class Tag(models.Model):
@@ -90,22 +90,22 @@ class Cta(models.Model):
 
 
 class Rating(models.Model):
-	profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
-	place = models.ForeignKey(Place, on_delete=models.CASCADE)
+	profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
+	place = models.ForeignKey('Place', on_delete=models.CASCADE)
 	rating = models.IntegerField()
 	# need to add unique constraints to prevent multiple ratings from the same user?
 
 
 class Todo(models.Model):
-	profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
-	item = models.ForeignKey(Item, on_delete=models.CASCADE)
+	profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
+	item = models.ForeignKey('Item', on_delete=models.CASCADE)
 	order = models.IntegerField()
 	done = models.BooleanField(default=False)
 	# need to add unique constraints to prevent duplicate items?
 
 
 class Bookmark(models.Model):
-	profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
-	item = models.ForeignKey(Item, on_delete=models.CASCADE)
+	profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
+	item = models.ForeignKey('Item', on_delete=models.CASCADE)
 	datetime = models.DateTimeField(auto_now_add=True)
 	# need to add unique constraints to prevent duplicate items?
