934e003688d1f436c2067a7d38ba7fe7833bf35c
Michael Bond <bond@datawaslost.net>
2018-02-28 19:52:50 +0000

    API Structure

---
+++ api.py
@@ -0,0 +1,27 @@
+from . import models
+from django.contrib.auth.models import User
+from rest_framework import serializers, viewsets
+
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class ProfileSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.Profile
+        fields = ('user',)
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = models.Profile.objects.all()
+    serializer_class = ProfileSerializer
