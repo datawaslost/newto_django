0d5843fda8444e468e2bd431ea5767cb935f839c
Michael Bond <bond@datawaslost.net>
2018-02-28 19:26:15 +0000

    Admin List Options

--- admin.py
+++ admin.py
@@ -14,8 +14,14 @@ class DefaultInline(admin.TabularInline):
 	ordering = ['order']
 
 
-class TipInline(admin.TabularInline):
+class MetroTipInline(admin.TabularInline):
 	model = Metro.tips.through
+	extra = 1
+	verbose_name = "tip"
+
+
+class OrganizationTipInline(admin.TabularInline):
+	model = Organization.tips.through
 	extra = 1
 	verbose_name = "tip"
 
@@ -23,29 +29,74 @@ class TipInline(admin.TabularInline):
 @admin.register(Metro)
 class MetroAdmin(admin.ModelAdmin):
 	exclude = ('tips',)
-	inlines = (DiscoverInline, DefaultInline, TipInline)
+	list_display = ('name', 'public')
+	inlines = (DiscoverInline, DefaultInline, MetroTipInline)
+
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+	exclude = ('tips',)
+	list_display = ('name', 'metro', 'public')
+	inlines = (DiscoverInline, DefaultInline, OrganizationTipInline)
 
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
 	exclude = ('next',)
-	list_display = ('name', 'metro', 'category', 'featured')
-    list_filter = ('metro', 'category')
+	list_display = ('name', 'metros', 'categories', 'featured')
+	list_filter = ('metro', 'category')
+	
+	def metros(self, obj):
+		return list(obj.metro.all())
+
+	def categories(self, obj):
+		return list(obj.category.all())
 
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
-	def queryset(self, request):
-		qs = super(ItemAdmin, self).queryset(request)
-		qs.filter(place=None)
-		return qs
+	list_display = ('name', 'content', 'sponsor', 'public')
+	def get_queryset(self, request):
+		qs = super(ItemAdmin, self).get_queryset(request)
+		return qs.filter(place=None)
 
 
-admin.site.register(Profile)
-admin.site.register(ProspectiveUser)
-admin.site.register(Organization)
-admin.site.register(Tip)
-admin.site.register(Group)
-admin.site.register(Category)
-admin.site.register(Tag)
-admin.site.register(Cta)
+@admin.register(ProspectiveUser)
+class ProspectiveUserAdmin(admin.ModelAdmin):
+	list_display = ('email', 'organization')
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+	list_display = ('user', 'metro', 'organization', 'joined', 'last_change')
+
+
+@admin.register(Tip)
+class TipAdmin(admin.ModelAdmin):
+	list_display = ('name', 'content')
+
+
+@admin.register(Cta)
+class CtaAdmin(admin.ModelAdmin):
+	list_display = ('name', 'link')
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+	list_display = ('name', 'items_list')
+	
+	def items_list(self, obj):
+		return list(obj.items.all())
+
+
+@admin.register(Category)
+class GroupAdmin(admin.ModelAdmin):
+	list_display = ('name', 'tags_list')
+	
+	def tags_list(self, obj):
+		return list(obj.tags.all())
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+	pass
