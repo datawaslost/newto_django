6465ebccad67d819489d4d1fea160297798aaec7
Michael Bond <bond@datawaslost.net>
2018-02-28 18:16:39 +0000

    Admin model tweaks

--- admin.py
+++ admin.py
@@ -1,37 +1,43 @@
 from django.contrib import admin
 from .models import *
 
-class ProfileAdmin(admin.ModelAdmin):
-    pass
-admin.site.register(Profile, ProfileAdmin)
-
-admin.site.register(ProspectiveUser)
-admin.site.register(Organization)
-admin.site.register(Tip)
-admin.site.register(Item)
-admin.site.register(Group)
-admin.site.register(Place)
-admin.site.register(Category)
-admin.site.register(Tag)
-admin.site.register(Cta)
 
 class DiscoverInline(admin.TabularInline):
 	model = Discover
 	extra = 1
 	ordering = ['order']
 
+
 class DefaultInline(admin.TabularInline):
 	model = Default
 	extra = 1
 	ordering = ['order']
 
+
 class TipInline(admin.TabularInline):
 	model = Metro.tips.through
 	extra = 1
 	verbose_name = "tip"
+
     
 class MetroAdmin(admin.ModelAdmin):
 	exclude = ('tips',)
 	inlines = (DiscoverInline, DefaultInline, TipInline)
 
+
+class PlaceAdmin(admin.ModelAdmin):
+	exclude = ('next',)
+
+
 admin.site.register(Metro, MetroAdmin)
+admin.site.register(Place, PlaceAdmin)
+
+admin.site.register(Profile)
+admin.site.register(ProspectiveUser)
+admin.site.register(Organization)
+admin.site.register(Tip)
+admin.site.register(Item)
+admin.site.register(Group)
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Cta)
